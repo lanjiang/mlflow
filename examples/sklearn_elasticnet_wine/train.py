bba64263608a4e231ac2c89f44387c9a6696d272
Lan Jiang <ljiang2@gmail.com>
2019-04-05 13:55:45 -0500

    Update train.py

--- examples/sklearn_elasticnet_wine/train.py
+++ examples/sklearn_elasticnet_wine/train.py
@@ -43,7 +43,10 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
+    
+    mlflow.set_tracking_uri('databricks')
+    mlflow.set_experiment('experiments/wine')
+    
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
